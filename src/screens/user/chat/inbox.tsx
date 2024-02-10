ee60ae7ed49f8465975500f8ae6d56f364e51574
ibrahim taofeek <taofeekibrahimope@gmail.com>
2024-02-10 15:34:50 +0100

    search on order page fixed

--- src/screens/user/chat/inbox.tsx
+++ src/screens/user/chat/inbox.tsx
@@ -162,6 +162,9 @@ export default function Inbox({navigation, route}: any) {
     }
   };
 
+
+  // pureworkerapp@gmail.com
+
   const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
 
   useEffect(() => {
