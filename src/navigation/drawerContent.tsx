6e98843ba07a70cdf7990f2351398866e65f501f
Bamgboye Taiwo <bamgboyetaiwo9@gmail.com>
2024-02-26 10:41:53 +0100

    changes

--- src/navigation/drawerContent.tsx
+++ src/navigation/drawerContent.tsx
@@ -413,7 +413,7 @@ const DrawerContent = () => {
           )}
           <View style={[tw`mt-4 ml-3`, {}]}>
             <Textcomp
-              text={`Version: ${Platform.OS === 'ios' ? '1.0.4.5' : '1.0.4.5'}`}
+              text={`Version: ${Platform.OS === 'ios' ? '1.0.4.6' : '1.0.4.6'}`}
               size={14}
               color={'#000000'}
               style={[
